197401fd4eb6721e5bf45cb5b86f01c6080a3a86
Lucas Scarelli <100313411+LucasScarelli@users.noreply.github.com>
2022-03-09 17:11:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let ans_head = ['head','hair','forehead','eyebrow','eyes','eyelashes','nose','cheek','beard','mouth','lip','teeth','tongue','jaw']
+
+function confirmar() {
+    let resp_hd = document.querySelectorAll('.input_head')
+
+    for (let i=0; i < resp_hd.length; i++){
+            console.log(resp_hd[i].value)
+    }
+}
+
+
+
+
+
+
+
+
